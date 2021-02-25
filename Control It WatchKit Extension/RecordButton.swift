1601258b242e2366e65801f1486d76a99c573636
Leonardo Viana <sonleo2013@gmail.com>
2021-02-25 13:21:25 -0400

    create custom button for recording audio

---
+++ Control It WatchKit Extension/RecordButton.swift
@@ -0,0 +1,40 @@
+//
+//  RecordButton.swift
+//  Control It WatchKit Extension
+//
+//  Created by Leonardo Viana on 25/02/21.
+//
+
+import SwiftUI
+
+struct RecordButton: View {
+    @Binding var isRecording: Bool
+    var onRecord: () -> Void
+    var onStop: () -> Void
+    
+    var body: some View {
+        Button(action: {
+            if isRecording {
+                onStop()
+            } else {
+                onRecord()
+            }
+        }, label: {
+            ZStack {
+                Circle()
+                    .strokeBorder(Color.black,lineWidth: 5)
+                    .background(
+                        Circle().foregroundColor(isRecording ? .black : .red)
+                    )
+                if isRecording {
+                    Rectangle()
+                        .frame(width: 60, height: 60)
+                        .cornerRadius(10)
+                        .foregroundColor(.red)
+                }
+            }
+        })
+        .clipShape(Circle())
+        .background(Circle().foregroundColor(.gray))
+    }
+}
