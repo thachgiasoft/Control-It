8af83ce6a1b22dccb08d7f9cb483fa526339c873
Ingra Cristina de Oliveira Morais <ingracristinaom@gmail.com>
2021-02-26 10:58:26 -0400

    register moods in progress

---
+++ RegisterMood.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterMood.swift
+//  Control It WatchKit Extension
+//
+//  Created by Ingra Cristina on 26/02/21.
+//
+
+import SwiftUI
+
+struct RegisterMood: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RegisterMood_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterMood()
+    }
+}
