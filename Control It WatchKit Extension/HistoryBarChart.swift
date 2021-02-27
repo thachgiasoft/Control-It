8c30a2b6ada95b370dfd79e005745e0f8ee70753
ivanportales <gidsp.snf19@uea.edu.br>
2021-02-27 19:35:15 -0400

    first implementation of the StatisticsViewModel,its not finished yet

--- Control It WatchKit Extension/HistoryBarChart.swift
+++ Control It WatchKit Extension/HistoryBarChart.swift
@@ -11,7 +11,7 @@ struct HistoryBarChart : View  {
     var yLabels : [Int]!
     var xLabels : [String]!
     var barHeights : [CGFloat]!
-    var maxHeight : Double!
+    var maxHeight : CGFloat!
     var squareSize : CGFloat!
     
     init(yLabels : [Int], xLabels : [String], barHeights : [CGFloat]) {
@@ -19,46 +19,48 @@ struct HistoryBarChart : View  {
         self.yLabels = yLabels.reversed()
         self.xLabels = xLabels
         self.barHeights = barHeights
-        self.maxHeight = Double(barHeights.max() ?? 0)
+        self.maxHeight = CGFloat(barHeights.max() ?? 0)
     }
     
     var body : some View {
-        GeometryReader { firstReader in
-            let fullHeight = firstReader.size.height
-            let fullWidth = firstReader.size.width
-            let lineHeight = (fullHeight - 20) / CGFloat(maxHeight)
-            
-            VStack {
-                HStack {
-                    VStack(spacing:0) {
-                        ForEach(0..<yLabels.count - 1) { index in
-                            ZStack(alignment: Alignment.top, content: {
-                                Rectangle().foregroundColor(.black)
-                                Text("\(yLabels[index])").padding(-3)
-                            }).frame(height: lineHeight * 5)
-                        }
-                        Text("0").frame(height: 20)
-                    }.frame(width: fullWidth * 0.15, height: fullHeight)
-                    GeometryReader { secondReader in
-                        let spacingBetweenBars : CGFloat = 8
-                        let barWidth = (secondReader.size.width / CGFloat(numbersOfBars)) - spacingBetweenBars
-                        
-                        HStack(spacing:spacingBetweenBars) {
-                            ForEach(0..<numbersOfBars) { index in
-                                VStack(spacing: 0) {
-                                    if barHeights[index] != CGFloat(maxHeight) {
-                                        Spacer().padding(0)
+        ZStack {
+            RoundedRectangle(cornerRadius: 10).foregroundColor(.black)
+            GeometryReader { firstReader in
+                let fullHeight = firstReader.size.height
+                let fullWidth = firstReader.size.width
+                let lineHeight = (fullHeight - 20) / maxHeight
+                VStack {
+                    HStack {
+                        VStack(spacing:0) {
+                            ForEach(0..<yLabels.count - 1) { index in
+                                ZStack(alignment: Alignment.top, content: {
+                                    RoundedRectangle(cornerRadius: 10).foregroundColor(.black)
+                                    Text("\(yLabels[index])").padding(-4)
+                                }).frame(height: lineHeight * 5).padding(.leading)
+                            }
+                            Text("0").frame(height: 20).padding(.leading)
+                        }.frame(width: fullWidth * 0.15, height: fullHeight)
+                        GeometryReader { secondReader in
+                            let spacingBetweenBars : CGFloat = 8
+                            let barWidth = (secondReader.size.width / CGFloat(numbersOfBars)) - spacingBetweenBars
+                            
+                            HStack(spacing:spacingBetweenBars) {
+                                ForEach(0..<numbersOfBars) { index in
+                                    VStack(spacing: 0) {
+                                        if barHeights[index] != maxHeight {
+                                            Spacer().padding(0)
+                                        }
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .fill(Color.purple)
+                                            .frame(width: barWidth, height: barHeights[index] * lineHeight).padding(0)
+                                        Text(xLabels[index]).frame(height: 20)
                                     }
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .fill(Color.purple)
-                                        .frame(width: barWidth, height: barHeights[index] * lineHeight).padding(0)
-                                    Text(xLabels[index]).frame(height: 20)
                                 }
                             }
-                        }
-                    }.frame(width: fullWidth * 0.85, height: fullHeight)
-                }.frame(width: fullWidth, height: fullHeight)
-            }
+                        }.frame(width: fullWidth * 0.85, height: fullHeight)
+                    }.frame(width: fullWidth, height: fullHeight)
+                }
+            }.padding()
         }
     }
 }
