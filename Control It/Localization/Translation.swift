4c04d86b226b213fd994e68d376ec4fdba94e070
Leonardo Viana <sonleo2013@gmail.com>
2021-03-05 12:05:43 -0400

    start localization of the iOS app

---
+++ Control It/Localization/Translation.swift
@@ -0,0 +1,18 @@
+//
+//  Translation.swift
+//  Control It
+//
+//  Created by Leonardo Viana on 05/03/21.
+//
+
+import Foundation
+
+public class Translation {
+    public class ViewTitles {
+        static let records = NSLocalizedString("Records", comment: "")
+        static let record = NSLocalizedString("Record", comment: "")
+        static let statistics = NSLocalizedString("Statistics", comment: "")
+        static let settings = NSLocalizedString("Settings", comment: "")
+        static let review = NSLocalizedString("Review", comment: "")
+    }
+}
